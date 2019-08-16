d5e99165a90b2c7501078f2695b48c42b8496ba0
Anu KC <anukc2000@gmail.com>
2019-08-16 09:50:20 -0500

    before pulling master

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -52,33 +52,6 @@ class Landing extends Component {
 
 
 
-           {/* -------------------------------------------------*/}
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
-            </div>
-            <div className="col s6">
-              <Link
-                to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                Log In
-              </Link>
-            </div>
           </div>
         </div>
       </div>
