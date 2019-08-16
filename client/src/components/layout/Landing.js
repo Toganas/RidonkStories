4658f8249f878c65a56a920fbf8b0c6eec9b3829
Anu KC <anukc2000@gmail.com>
2019-08-16 10:27:52 -0500

    front end ready to send info to backend

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { Jumbotron, Container, Breadcrumb, BreadcrumbItem } from 'react-bootstrap';
 
 class Landing extends Component {
