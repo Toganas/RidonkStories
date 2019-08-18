4f5accf56ecc73cd96f2e3a3dc7326093ff725c6
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-17 20:23:08 -0500

    we now have Story instead of Book for findAll.  changed the db require to models instead of models/Story.  All other changes were done by my formatter(sorry?)

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,25 +1,26 @@
-const db = require("../models/Story");
+const db = require("../models");
 
 module.exports = {
-    findAll: function(req, res) {
-        db.Book
-          .find(req.query)
-          .sort({ date: -1 })
-          .then(dbModel => res.json(dbModel))
-          .catch(err => res.status(422).json(err));
-      },
-
-    create: function(req, res) {
-        db.Story 
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+    findAll: function (req, res) {
+        db.Story
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     },
 
-    remove: function(req,res) {
+    create: function (req, res) {
         db.Story
-        .findById({_id: req.params.id})
-        .then(dbModel => dbModel.remove())
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
-    }}
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    remove: function (req, res) {
+        db.Story
+            .findById({ _id: req.params.id })
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+}
