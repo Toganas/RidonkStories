22831491511132b905ab52777b0f8a29d293e32a
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-18 15:44:21 -0500

    pretty sure this is all formatting, but I'm not 100% sure, and this works, so I'm not risking it.

--- client/src/actions/storiesActions.js
+++ client/src/actions/storiesActions.js
@@ -1,20 +1,20 @@
-import {FETCH_STORIES, CREATE_STORIES, DELETE_STORIES} from './types';
+import { FETCH_STORIES, CREATE_STORIES, DELETE_STORIES } from './types';
 import axios from "axios";
 import history from "../history";
 
 export const createStories = formValues => {
     return async (dispatch, getState) => {
-        const {user} = getState().auth;
-        const response = await axios.post("/api/story/", {...formValues, user});
-        dispatch ({ type: CREATE_STORIES, payload: response.data})
+        const { user } = getState().auth;
+        const response = await axios.post("/api/story/", { ...formValues, user });
+        dispatch({ type: CREATE_STORIES, payload: response.data })
     }
 };
 
 export const fetchStories = () => {
     return async dispatch => {
-        
-        const response = await axios.get( "/api/story/")
-        dispatch ({ type: FETCH_STORIES, payload: response.data});
+
+        const response = await axios.get("/api/story/")
+        dispatch({ type: FETCH_STORIES, payload: response.data });
     }
 };
 
@@ -23,7 +23,7 @@ export const fetchStories = () => {
 export const deleteStories = id => {
     return async dispatch => {
         await axios.delete(`/api/story/${id}`);
-        dispatch ({ type: DELETE_STORIES, payload: id});
+        dispatch({ type: DELETE_STORIES, payload: id });
         history.push("/")
     }
 }
