5a72d451a45b9c39a55859779dc646ee31b99f4a
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-18 21:54:22 -0500

    sorted by inverse order of when it was made

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -4,6 +4,7 @@ module.exports = {
     findAll: function (req, res) {
         Story
             .find({})
+            .sort({ date: -1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
