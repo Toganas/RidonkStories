d903c98c04e978e8f6c64872f037cbc1166b96c5
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-19 15:35:24 -0500

    resolved conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from "./App";
-
 import "./index.css";
-// import reducers from './reducers';
+import reducers from './reducers';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
