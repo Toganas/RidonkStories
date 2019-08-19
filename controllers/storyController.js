597ac775dfe14eaef56f35cabb425bc55ce6d482
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-18 22:54:39 -0500

    created a route for finding category.   Unsure that it'll actually work at the moment

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -20,5 +20,11 @@ module.exports = {
             .then(dbModel => dbModel.remove())
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
+    },
+    findCat: function (req, res) {
+        Story
+            .find({ category: req.params.cat })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
     }
 }
