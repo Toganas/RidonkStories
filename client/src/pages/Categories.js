9d874790ab62796761f619a9f62f35dc25d6d919
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-18 22:55:42 -0500

    started created a Categories page, then realized I was confused.   copied form the Stories of the Day page

--- client/src/pages/Categories.js
+++ client/src/pages/Categories.js
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import { Card } from 'react-bootstrap';
+import StoryAPI from "../utils/StoryAPI";
 
 
 
-export const Categories = () => (
+class Categories extends Component {
+  state = {
+    stories: [],
+    title: "",
+    story: "",
+    lol: "",
+    favorites: "",
 
-  <div>
+  };
+
+  componentDidMount() {
+    this.loadStories();
+  }
+
+  loadStories = () => {
+    StoryAPI.getStory()
+      .then(res =>
+        this.setState({ stories: res.data, title: "", story: "" })
+      ).catch(err => console.log(err));
+
+  };
+
+  render() {
+    return (
+      <div className="container">
+
+        <div className="StoriesDay">
+          {this.state.stories.map(str => (
 
 
-  </div>
+            <Card style={{ width: 'auto' }} key={str._id}>
+              <Card.Body >
+                <Card.Title className="title">
+                  {str.title}
+                </Card.Title>
 
-)
+                <Card.Text className="story">
+                  {str.story}
+                </Card.Text>
+
+                <Card.Footer className="category">
+                  <small>Category: {str.category}</small>
+                </Card.Footer>
+
+              </Card.Body>
+            </Card>
+
+          ))}
+
+
+
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default Categories;
