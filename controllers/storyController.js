b40d0c200ffe0cb63ec1ade3bcef2bb57e4f27aa
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-19 15:04:48 -0500

    sort by most recently added

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -24,6 +24,7 @@ module.exports = {
     findCat: function (req, res) {
         Story
             .find({ category: req.params.cat })
+            .sort({ date: -1 })
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     }
